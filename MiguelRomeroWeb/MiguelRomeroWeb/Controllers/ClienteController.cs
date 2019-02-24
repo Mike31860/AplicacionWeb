345924a9c19e843a6e4cd9173a0d37922cb4ede9
Miguel Romero <miguel980429@gmail.com>
2019-02-23 22:33:01 -0500

    Working

---
+++ MiguelRomeroWeb/MiguelRomeroWeb/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MiguelRomeroWeb.Controllers
+{
+    public class ClienteController : Controller
+    { // 
+      // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
